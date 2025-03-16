2efd18a6bb2111a8bed9e73a713f3dbb8ca725a9
Rami Aj <120121004+killer225971@users.noreply.github.com>
2025-03-16 19:14:10 +0200

    i updated the comments

--- DOWNLOADS/LINUX_COURSE_WORK-downloads/plant.py
+++ DOWNLOADS/LINUX_COURSE_WORK-downloads/plant.py
@@ -6,7 +6,7 @@ height_data = [50, 55, 60, 65, 70]  # Height data over time (in cm)
 leaf_count_data = [35, 40, 45, 50, 55]  # Leaf count over time
 dry_weight_data = [2.0, 2.0, 2.1, 2.1, 3.0]  # Dry weight over time (in grams)
 
-# Print out the plant data (optional)
+# Print out the plant data 
 print(f"Plant: {plant}")
 print(f"Height data: {height_data} cm")
 print(f"Leaf count data: {leaf_count_data}")
